fa4ab6324e20278fab20c52c14697cb6c7fd3acd
Robert Prehn <robert@revelry.co>
2018-07-13 14:34:23 -0500

    fix: Remove dead code linters JSHint and JSCS

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
-var Linters = {
-  JSXJSHint: require('./linters/jsxjshint.js'),
-  JSXCheckStyle: require('./linters/jsxjscs.js'),
-  CoffeeLint: require('./linters/cjsx.js')
+const Linters = {
+  CoffeeLint: require('./linters/cjsx.js'),
 };
 
 module.exports = global.Linters = Linters;
